e6a0ab3c66a3a815d3e9c372d0625c7fdb6d0ee2
Carlos <ferreira.carlos.e@gmail.com>
2018-06-06 09:30:47 -0400

    update

--- storage/framework/views/d2ba57ba4a5953b9deb01e6b025c969a66740062.php
+++ storage/framework/views/d2ba57ba4a5953b9deb01e6b025c969a66740062.php
@@ -85,77 +85,8 @@
         </div>
     </div>
     <!-- /top tiles -->
-
-    <div class="row">
-        <div class="col-md-12 col-sm-12 col-xs-12">
-            <div id="log_activity" class="dashboard_graph">
-
-                <div class="row x_title">
-                    <div class="col-md-6">
-                        <h3><?php echo e(__('views.admin.dashboard.sub_title_0')); ?></h3>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="date_piker pull-right"
-                             style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
-                            <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
-                            <span class="date_piker_label">
-                                <?php echo e(\Carbon\Carbon::now()->addDays(-6)->format('F j, Y')); ?> - <?php echo e(\Carbon\Carbon::now()->format('F j, Y')); ?>
-
-                            </span>
-                            <b class="caret"></b>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-9 col-sm-9 col-xs-12">
-                    <div class="chart demo-placeholder" style="width: 100%; height:360px;"></div>
-                </div>
-
-
-                <div class="col-md-3 col-sm-3 col-xs-12 bg-white">
-                    <div class="x_title">
-                        <h2><?php echo e(__('views.admin.dashboard.sub_title_1')); ?></h2>
-                        <div class="clearfix"></div>
-                    </div>
-
-                    <div class="col-md-12 col-sm-12 col-xs-6">
-                        <div>
-                            <p><?php echo e(__('views.admin.dashboard.log_level_0')); ?></p>
-                            <div class="">
-                                <div class="progress progress_sm" style="width: 76%;">
-                                    <div class="progress-bar log-info" role="progressbar" data-transitiongoal="0"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <p><?php echo e(__('views.admin.dashboard.log_level_1')); ?></p>
-                            <div class="">
-                                <div class="progress progress_sm" style="width: 76%;">
-                                    <div class="progress-bar log-notice" role="progressbar" data-transitiongoal="0"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <p><?php echo e(__('views.admin.dashboard.log_level_2')); ?></p>
-                            <div class="">
-                                <div class="progress progress_sm" style="width: 76%;">
-                                    <div class="progress-bar log-warning" role="progressbar" data-transitiongoal="0"></div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="clearfix"></div>
-            </div>
-        </div>
-
-    </div>
-    <br />
-
-    Good Graph
-
+    
+    <!-- 
     <div class="row">
         <div class="col-md-4 col-sm-4 col-xs-12">
             <div id="registration_usage" class="x_panel tile fixed_height_320 overflow_hidden">
@@ -246,6 +177,77 @@
             </div>
         </div>
     </div>
+    -->
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div id="log_activity" class="dashboard_graph">
+
+                <div class="row x_title">
+                    <div class="col-md-6">
+                        <h3><?php echo e(__('views.admin.dashboard.sub_title_0')); ?></h3>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="date_piker pull-right"
+                             style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
+                            <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
+                            <span class="date_piker_label">
+                                <?php echo e(\Carbon\Carbon::now()->addDays(-6)->format('F j, Y')); ?> - <?php echo e(\Carbon\Carbon::now()->format('F j, Y')); ?>
+
+                            </span>
+                            <b class="caret"></b>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-9 col-sm-9 col-xs-12">
+                    <div class="chart demo-placeholder" style="width: 100%; height:360px;"></div>
+                </div>
+
+
+                <div class="col-md-3 col-sm-3 col-xs-12 bg-white">
+                    <div class="x_title">
+                        <h2><?php echo e(__('views.admin.dashboard.sub_title_1')); ?></h2>
+                        <div class="clearfix"></div>
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-6">
+                        <div>
+                            <p><?php echo e(__('views.admin.dashboard.log_level_0')); ?></p>
+                            <div class="">
+                                <div class="progress progress_sm" style="width: 76%;">
+                                    <div class="progress-bar log-info" role="progressbar" data-transitiongoal="0"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <p><?php echo e(__('views.admin.dashboard.log_level_1')); ?></p>
+                            <div class="">
+                                <div class="progress progress_sm" style="width: 76%;">
+                                    <div class="progress-bar log-notice" role="progressbar" data-transitiongoal="0"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <p><?php echo e(__('views.admin.dashboard.log_level_2')); ?></p>
+                            <div class="">
+                                <div class="progress progress_sm" style="width: 76%;">
+                                    <div class="progress-bar log-warning" role="progressbar" data-transitiongoal="0"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+    </div>
+    <br />
+
+    
 
     
 
