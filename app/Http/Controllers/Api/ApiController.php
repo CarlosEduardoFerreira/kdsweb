0fec0064de2af52e95374253b0b57177bce5c3ed
Carlos <ferreira.carlos.e@gmail.com>
2018-06-13 12:02:34 -0400

    Api Connection improvement

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -38,19 +38,23 @@ class ApiController extends Controller
             
             if($req == "SYNC") {
 
-                $response = ApiSyncController::InsertOrUpdateEntityWeb($request, $response);
+                $syncController = new ApiSyncController();
+                $response = $syncController::InsertOrUpdateEntityWeb($request, $response);
 
             } else if($req == "LOGIN") {
 
-                $response = ApiUserController::login($request, $response);
+                $userController = new ApiUserController();
+                $response = $userController::login($request, $response);
 
             } else if($req == "GET_SETTINGS") {
                 
-                $response = ApiSettingsController::getSettings($request, $response);
+                $settingsController = new ApiSettingsController();
+                $response = $settingsController::getSettings($request, $response);
                 
             } else if($req == "GET_DEVICES") {
 
-                $response = ApiDeviceController::getDevices($request, $response);
+                $deviceController = new ApiDeviceController();
+                $response = $deviceController::getDevices($request, $response);
                 
             }
             
