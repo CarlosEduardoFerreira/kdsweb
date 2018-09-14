b4a0180d61c2f5f755d009d10584bb3eff5b0051
Carlos <ferreira.carlos.e@gmail.com>
2018-09-14 10:30:58 -0400

    DateTime class

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -12,8 +12,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use DateTime;
-use DateTimeZone;
+use Faker\Provider\DateTime;
 use Ramsey\Uuid\Uuid;
 
 
